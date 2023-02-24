22a5925288714f8a3b90bcdecec3a9282cf8b6cb
Norbert Frank Mba <norbertmbafrank@gmail.com>
2023-02-24 11:45:15 +0100

    implement borrowing from pancake swap

---
+++ contracts/PancakeFlashSwap.sol
@@ -0,0 +1,140 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.6.6;
+import "hardhat/console.sol";
+
+// import libraries and interfaces
+import "./libraries/SafeERC20.sol";
+import "./libraries/SafeMath.sol";
+
+import "./libraries/UniswapV2Library.sol";
+import "./interfaces/IERC20.sol";
+import "./interfaces/IUniswapV2Factory.sol";
+import "./interfaces/IUniswapV2Pair.sol";
+import "./interfaces/IUniswapV2Router01.sol";
+import "./interfaces/IUniswapV2Router02.sol";
+
+contract PancakeFlashSwap {
+    using SafeERC20 for IERC20;
+    using SafeMath for uint256;
+    // factory and router  contract addresses
+    address private constant PANCAKEV2_FACTORY =
+        0xcA143Ce32Fe78f1f7019d7d551a6402fC5350c73;
+    address private constant PANCAKEV2_ROUTER =
+        0x10ED43C718714eb63d5aA57B78B54704E256024E;
+
+    // token contract addresses
+
+    address private constant WBNB = 0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c;
+    address private constant BUSD = 0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56;
+    address private constant CAKE = 0x0E09FaBB73Bd3Ade0a17ECC321fD13a19e81cE82;
+    address private constant USDT = 0x55d398326f99059fF775485246999027B3197955;
+    address private constant CROX = 0x2c094F5A7D1146BB93850f629501eB749f6Ed491;
+
+    // set Deadline
+    uint256 private deadline = block.timestamp;
+    uint256 private constant MAX_INT =
+        115792089237316195423570985008687907853269984665640564039457584007913129639935;
+
+    // Fund smart contract
+    // function allows smart contract to be funded
+
+    function fundFlashContract(
+        address _owner,
+        address _token,
+        uint256 _amount
+    ) public {
+        IERC20(_token).transferFrom(_owner, address(this), _amount);
+    }
+
+    // get balance of token on contract
+    function getFlashContractBalance(address _token)
+        public
+        view
+        returns (uint256)
+    {
+        return IERC20(_token).balanceOf(address(this));
+    }
+
+    // get flashloan from contract
+    function startLoan(address _tokenBorrow, uint256 _amount) external {
+        IERC20(BUSD).approve(address(PANCAKEV2_ROUTER), MAX_INT);
+        IERC20(CAKE).approve(address(PANCAKEV2_ROUTER), MAX_INT);
+        IERC20(USDT).approve(address(PANCAKEV2_ROUTER), MAX_INT);
+        IERC20(CROX).approve(address(PANCAKEV2_ROUTER), MAX_INT);
+
+        // get the factory pair address for combined
+        address pair = IUniswapV2Factory(PANCAKEV2_FACTORY).getPair(
+            _tokenBorrow,
+            WBNB
+        );
+
+        require(pair != address(0), "pool doesn't exist");
+
+        // get pair
+        address token0 = IUniswapV2Pair(pair).token0();
+        address token1 = IUniswapV2Pair(pair).token1();
+        uint256 amountOut0 = _tokenBorrow == token0 ? _amount : 0;
+        uint256 amountOut1 = _tokenBorrow == token1 ? _amount : 0;
+
+        // encode data
+        bytes memory data = abi.encode(_tokenBorrow, _amount);
+
+        // call swap
+        IUniswapV2Pair(pair).swap(amountOut0, amountOut1, address(this), data);
+    }
+
+    function getPairBalance() public view returns(uint256, uint256, uint256, uint256){
+        address pair = IUniswapV2Factory(PANCAKEV2_FACTORY).getPair(
+            BUSD,
+            WBNB
+        );
+
+        uint256 balance0 = IERC20(BUSD).balanceOf(IUniswapV2Pair(pair).token0());
+        uint256 balance1 = IERC20(WBNB).balanceOf(IUniswapV2Pair(pair).token1());
+        uint256 balance2 = IERC20(BUSD).balanceOf(address(pair));
+        uint256 balance3 = IERC20(BUSD).balanceOf(address(this));
+        // console.log("balance0: %s, balance1: %s, balance2: %s, balance3: %s", balance0, balance1, balance2, balance3);
+        return (balance0, balance1, balance2, balance3);
+    }
+
+    function pancakeCall(
+        address _sender,
+        uint256 _amount0,
+        uint256 _amount1,
+        bytes calldata _data
+    ) external {
+        address token0 = IUniswapV2Pair(msg.sender).token0();
+        address token1 = IUniswapV2Pair(msg.sender).token1();
+
+        address pair = IUniswapV2Factory(PANCAKEV2_FACTORY).getPair(
+            token0,
+            token1
+        );
+        require(pair == msg.sender, "pair does not exist");
+        require(
+            _sender == address(this),
+            "Swap call was not called by this contract"
+        );
+
+        // decode data
+        (address _tokenBorrow, uint256 _amount) = abi.decode(
+            _data,
+            (address, uint256)
+        );
+
+        // calculate amount to repay
+        uint256 fee = ((_amount * 3) / 997) + 1;
+        uint256 amountToRepay = _amount.add(fee);
+
+        // Perform arbitrage
+
+        // Pay yourself
+        console.log("SOL: check amount %s, balance: %s", amountToRepay,  IERC20(BUSD).balanceOf(address(this)));
+        // Pay back loan
+        IERC20(_tokenBorrow).transferFrom(
+            address(this),
+            pair,
+            amountToRepay
+        );
+    }
+}
--- test/tester.js
+++ test/tester.js
@@ -5,7 +5,6 @@ const { impersonateFundErc20 } = require("../utils/utilities");
 const {
   abi,
 } = require("../artifacts/contracts/interfaces/IERC20.sol/IERC20.json");
-const { inputToConfig } = require("@ethereum-waffle/compiler");
 
 const provider = waffle.provider;
 
@@ -21,7 +20,7 @@ describe("FlashSwap Contract", () => {
 
   const BUSD_WHALE = "0x8894e0a0c962cb723c1976a4421c95949be2d4e3";
   const WBNB = "0xbb4cdb9cbd36b01bd1cbaebf2de08d9173bc095c";
-  const BUSD = "0xe9e7cea3dedca5984780bafc599bd69add087d56";
+  const BUSD = "0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56";
   const CAKE = "0x0e09fabb73bd3ade0a17ecc321fd13a19e81ce82";
   const USDT = "0x55d398326f99059ff775485246999027b3197955";
   const CROX = "0x2c094f5a7d1146bb93850f629501eb749f6ed491";
@@ -32,17 +31,19 @@ describe("FlashSwap Contract", () => {
 
   beforeEach(async () => {
     // Get owner as signer
-    [owner] = await ethers.getSigners();
+    const [owner] = await ethers.getSigners();
 
     // Ensure that the WHALE has a balance
     const whale_balance = await provider.getBalance(BUSD_WHALE);
     expect(whale_balance).not.equal("0");
 
-    BORROW_AMOUNT = ethers.utils.parseUnits("1", DECIMALS);
-
-    initialFundingHuman = "100";
+    const amountToBorrowInHuman = "10";
+    BORROW_AMOUNT = ethers.utils.parseUnits(amountToBorrowInHuman, DECIMALS);
+    initialFundingHuman = "1000";
     FUND_AMOUNT = ethers.utils.parseUnits(initialFundingHuman, DECIMALS);
-    const flashSwapFactory = await ethers.getContractFactory("PancakeFlashSwap");
+    const flashSwapFactory = await ethers.getContractFactory(
+      "PancakeFlashSwap"
+    );
     FLASHSWAP = await flashSwapFactory.deploy();
     await FLASHSWAP.deployed();
 
@@ -55,14 +56,40 @@ describe("FlashSwap Contract", () => {
   });
 
   describe("Arbitrage Execution", () => {
-    it("ensures balance was successfully transferred", async () => {
-      const tokenBalance = await FLASHSWAP.getBalanceOfToken(BASE_TOKEN_ADDRESS);
+    it("ensures contract is funded", async () => {
+      const tokenBalance = await FLASHSWAP.getFlashContractBalance(
+        BASE_TOKEN_ADDRESS
+      );
+
+      const tokenBalances = await FLASHSWAP.getPairBalance();
+      console.log(
+        "CHECK THIS ",
+        ethers.utils.formatUnits(tokenBalances[0], DECIMALS),
+        ethers.utils.formatUnits(tokenBalances[1], DECIMALS),
+        ethers.utils.formatUnits(tokenBalances[2], DECIMALS),
+        ethers.utils.formatUnits(tokenBalances[3], DECIMALS),
+        tokenBalances[3].toString(),
+
+        " CHECK THIS"
+      );
       const tokenBalanceInHuman = ethers.utils.formatUnits(
         tokenBalance,
         DECIMALS
       );
-      console.log(tokenBalanceInHuman);
       expect(Number(tokenBalanceInHuman)).equal(Number(initialFundingHuman));
+    });
+
+    it("excutes an arbitrage", async () => {
+      txArbitrage = await FLASHSWAP.startLoan(BUSD, BORROW_AMOUNT);
+
+      const balanceAfterArbitrage = await FLASHSWAP.getFlashContractBalance(
+        BASE_TOKEN_ADDRESS
+      );
+      const formattedAmount = ethers.utils.formatUnits(
+        balanceAfterArbitrage,
+        DECIMALS
+      );
+      assert(txArbitrage);
     });
   });
 });
